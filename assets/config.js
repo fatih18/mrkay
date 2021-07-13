97289e40c0e0eff90d7664082bc989caf63b8d7e
fatih18 <60979952+fatih18@users.noreply.github.com>
2021-07-13 17:25:04 +0300

    Update config.js

--- assets/config.js
+++ assets/config.js
@@ -1,3 +1,9 @@
 const RASTER_URL = 'https://servis.pirireis.com.tr/57wms/service?wmtver=1.0.0?&SERVICE=WMS&VERSION=1.0.1&REQUEST=GetMap&SRS=EPSG:3857&LAYERS=kure_deniz_layer&STYLES='
 const MESH_URL = 'https://servis.pirireis.com.tr/webkure/csglobe'
 const GLOBEDLL_URL = 'https://servis.pirireis.com.tr/webkure/webglobe/webglobeserver.dll'
+const RASTER_URL21 =
+  "https://ows.mundialis.de/services/service?SERVICE=WMS&REQUEST=GetMap&SRS=EPSG:3857&LAYERS=OSM-WMS&STYLES=&VERSION=1.1.0";
+const RASTER_URL22 = "http://b.tile.openstreetmap.org/{z}/{x}/{y}.png";
+
+const RASTER_URL23 =
+  "https://api.mapbox.com/styles/v1/openaerialmap/ciyx28hy800362rto0u9x10fv/tiles/512/{z}/{x}/{y}?access_token=pk.eyJ1Ijoib3BlbmFlcmlhbG1hcCIsImEiOiJjaXl4MjM5c20wMDBmMzNucnZtbnYwZTcxIn0.IKG5flWCS6QfpO3iOdRveg";
