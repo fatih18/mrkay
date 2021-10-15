a987b1af102069f0ebce7f9b92443842fbcdda6c
fatih.akbayrak <fatih.akbayrak@pirireis.com.tr>
2021-10-15 09:16:03 +0300

    feat: add marker method

--- index.js
+++ index.js
@@ -17,7 +17,15 @@
 // });
 
 const express = require("express");
+const cors = require("cors");
+
 const app = express();
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+
 const port = 8085;
 
 app.get("/", function (req, res) {
